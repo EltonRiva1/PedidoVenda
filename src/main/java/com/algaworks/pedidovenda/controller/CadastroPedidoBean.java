49d3fcb19cff760cfe514f1846564f687b0fb8da
Elton Riva <notleamil@gmail.com>
2022-04-02 22:57:15 -0300

    first commit

---
+++ src/main/java/com/algaworks/pedidovenda/controller/CadastroPedidoBean.java
@@ -0,0 +1,22 @@
+package com.algaworks.pedidovenda.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CadastroPedidoBean {
+	private List<Integer> itens;
+
+	public CadastroPedidoBean() {
+		this.itens = new ArrayList<>();
+		this.itens.add(1);
+	}
+
+	public List<Integer> getItens() {
+		return this.itens;
+	}
+}
---
+++ src/main/java/com/algaworks/pedidovenda/controller/PesquisaPedidosBean.java
@@ -0,0 +1,24 @@
+package com.algaworks.pedidovenda.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaPedidosBean {
+	private List<Integer> pedidosFiltrados;
+
+	public PesquisaPedidosBean() {
+		this.pedidosFiltrados = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			this.pedidosFiltrados.add(i);
+		}
+	}
+
+	public List<Integer> getPedidosFiltrados() {
+		return this.pedidosFiltrados;
+	}
+}
